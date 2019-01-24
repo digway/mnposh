7cff2a2f1a5058194cb45a04ddcf06d4aca19282
Don Jacobs SD <JACODONSD@chrobinson.com>
2019-01-23 19:13:39 -0600

    Initial Creation

---
+++ Get-VeryCool.ps1
@@ -0,0 +1,26 @@
+Function Get-VeryCool {
+    <#
+        .Synopsis
+            I want to be cool.
+        .Description
+            If a woodchuck could chuck wood, how much wood could a woodchuck chuck, if a woodchuck could chuck wood.
+        .Example
+            Get-VeryCool -User Donald
+            Gets me very cool!
+        .Notes
+            Created on 2019-01-23
+        .Link
+            https://www.meetup.com/Twin-Cities-PowerShell-User-Group
+    #>
+    [CmdletBinding()]
+    Param (
+        [string[]]$User
+    )
+    Begin { }
+    Process {
+        Write-Host "$user is cool."
+        Write-Host "brrr."
+        Write-Host "Now, $user is very cool!"
+    }
+    End { }
+}
