c3f81bc82721ffb75f61b4bd0dd87a2bf006a0cd
Donald Jacobs <donaldigway@gmail.com>
2019-02-10 09:49:26 -0600

    Format change

--- Remote Time Zone and Time.ps1
+++ Remote Time Zone and Time.ps1
@@ -1,24 +1,24 @@
 <#
-    .Synopsis
-        How to get time zone from a remote computer.
-    .Description
-        Uses WMI to get Remote info from another computer about time zone and local time.
-    .Link
-        https://www.microsoft.com
-    .Link
-        Get-WmiObject
+	.Synopsis
+		How to get time zone from a remote computer.
+	.Description
+		Uses WMI to get Remote info from another computer about time zone and local time.
+	.Link
+		https://www.microsoft.com
+	.Link
+		Get-WmiObject
 #>
 [CmdletBinding()]
 Param (
-    $ComputerName
+	$ComputerName
 )
 Begin {
 
 }
 Progress {
-    $TimeZone = Get-WmiObject -Class Win32_TimeZone -ComputerName $computer
-    $RemoteTime = Get-WmiObject -Class Win32_LocalTime -ComputerName $computer
+	$TimeZone = Get-WmiObject -Class Win32_TimeZone -ComputerName $computer
+	$RemoteTime = Get-WmiObject -Class Win32_LocalTime -ComputerName $computer
 }
 End {
-    
+	# Bye
 }
